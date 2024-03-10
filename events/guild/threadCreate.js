4f4e4447142d5b7316f539467354a822f374d508
Sonu Jana <krishnajana295@gmail.com>
2024-03-10 18:53:40 +0530

    Fixed Errors

---
+++ events/guild/threadCreate.js
@@ -0,0 +1,18 @@
+//The Module
+module.exports = async (client, thread) => {
+    try{
+        if(thread.joinable && !thread.joined){
+            await thread.join();
+        }
+    }catch (e){
+        console.log(String(e).grey)
+    }
+}
+/**
+ * @INFO
+ * Bot Coded by sonujana#0 | https://discord.gg/theolympus69 
+ * @INFO
+ * Please mention Him / Team Olympus, when using this Code
+ * @INFO
+ * Instagram - https://instagram.com/sonujana.ig
+ */
