57226efbb14f0e306de68bde556709141e8a8984
Sonu Jana <krishnajana295@gmail.com>
2024-03-10 17:17:53 +0530

    Fixes and Improvements

---
+++ commands/📈 Ranking/levelinghelp.js
@@ -0,0 +1,26 @@
+//Here the command starts
+const config = require(`${process.cwd()}/botconfig/config.json`)
+var ee = require(`${process.cwd()}/botconfig/embed.json`)
+module.exports = {
+	//definition
+	name: "levelinghelp", //the name of the command 
+	category: "📈 Ranking", //the category this will be listed at, for the help cmd
+	aliases: [""], //every parameter can be an alias
+	cooldown: 4, //this will set it to a 4 second cooldown
+	usage: "levelinghelp", //this is for the help command for EACH cmd
+  	description: "Shows the help for leveling", //the description of the command
+	  type: "info",
+	//running the command with the parameters: client, message, args, user, text, prefix
+  run: async (client, message, args, cmduser, text, prefix) => {
+    
+
+	}
+}
+/**
+ * @INFO
+ * Bot Coded by sonujana#0 | https://discord.gg/theolympus69 
+ * @INFO
+ * Please mention Him / Team Olympus, when using this Code
+ * @INFO
+ * Instagram - https://instagram.com/sonujana.ig
+ */
